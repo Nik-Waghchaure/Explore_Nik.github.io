d724aed3a295e5b32879ccdbfffecbb8f5e871c0
Nik Waghchaure <waghchaureniks@gmail.com>
2022-01-29 11:56:57 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,20 +1,4 @@
 <?php
-    #$Name=$_POST[name];
-    #$email=$_POST[email];
-    #$Subject=$_POST[Subject];
-    #$Message=$_POST[message];
 
-    #$conn = new mysqli('localhost','root','','message');
-    #if($conn->connect_error){
-     #   die ('Connection Failed :' .$conn->connect_error);
-   # }else{
-    #    $stmt = $conn->prepare("insert into registration(name,email,subject,message) 
-     #   values(?, ?, ?, ?");
-     #   $stmt->bind_param("ssss",$name, $email, $Subject, $message);
-     #   $stmt->execute();
-        echo "Message Sent...";
-     #   $stmt->close();
-     #   $conn->close();
-    #}
 
 ?>
