7d844a5045aceb467576725278312003f315bf7f
Nik Waghchaure <98530405+Nik-Waghchaure@users.noreply.github.com>
2022-07-07 18:29:26 +0530

    Typewriter
    Changes made in Typing Texts..

--- script.js
+++ script.js
@@ -35,16 +35,16 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student", "Learner", "Developer", "Designer"],
-        typeSpeed: 100,
-        backSpeed: 60,
+        strings: ["Student", "Developer", "Designer"],
+        typeSpeed: 150,
+        backSpeed: 80,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student", "Learner","Developer", "Designer"],
-        typeSpeed: 100,
-        backSpeed: 60,
+        strings: ["Student", "Developer", "Designer"],
+        typeSpeed: 150,
+        backSpeed: 80,
         loop: true
     });
 
